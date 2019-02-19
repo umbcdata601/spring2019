312755c30c7432598151489ddad659c425db7022
Ben Payne <ben.is.located@gmail.com>
2019-02-18 16:31:30 -0800

    notebook cleanup

---
+++ jupyter_notebooks/remove_irrelevant_directories.sh
@@ -0,0 +1,6 @@
+find . -type d -name .ipynb_checkpoints -exec rm -rf {} \;
+find . -type d -name .ipython -exec rm -rf {} \;
+find . -type d -name .local -exec rm -rf {} \;
+find . -type d -name .jupyter -exec rm -rf {} \;
+find . -type d -name .cache -exec rm -rf {} \;
+find . -type d -name .config -exec rm -rf {} \;
