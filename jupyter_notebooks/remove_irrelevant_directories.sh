a4c70ad6030a53f0d485db35555ffe0606d20678
Ben Payne <ben.is.located@gmail.com>
2019-03-14 07:50:23 -0400

    updates

--- jupyter_notebooks/remove_irrelevant_directories.sh
+++ jupyter_notebooks/remove_irrelevant_directories.sh
@@ -3,4 +3,6 @@ find . -type d -name .ipython -exec rm -rf {} \;
 find . -type d -name .local -exec rm -rf {} \;
 find . -type d -name .jupyter -exec rm -rf {} \;
 find . -type d -name .cache -exec rm -rf {} \;
+find . -type d -name .npm -exec rm -rf {} \;
+find . -type d -name .DS_Store -exec -rf {} \;
 find . -type d -name .config -exec rm -rf {} \;
